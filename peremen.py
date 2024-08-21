9fc9f872bc6bc821d984feca5cd320f327e49f71
1rina410 <irina_nazarova_1980@mail.ru>
2024-08-21 18:55:52 +0500

    Add files via upload

---
+++ peremen.py
@@ -0,0 +1,14 @@
+first_name = 'Курс:Python,всего задач:'
+count = 12
+second_name ='затрачено часов'
+time_count = 1.5
+name = 'среднее время выполнения'
+time_per_task = time_count/count
+print (first_name,count,second_name,time_count,name,time_per_task)
+
+
+
+
+
+
+
